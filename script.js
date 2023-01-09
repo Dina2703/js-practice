e12bfc569473df713b3fff4a52ac746d8b095f99
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-01-08 19:50:03 -0500

    JS basic challenges

---
+++ script.js
@@ -0,0 +1,29 @@
+//Write a function that takes a value as argument. Return the type of the value.
+// function myFunc(a) {
+//   return console.log(typeof a);
+// }
+// console.log(myFunc(false));
+
+//------
+
+//Get nth character of string. Write a function that takes a string (a) and a number (n) as argument. Return the nth character of 'a'.
+// function myFunction(a, n) {
+//   return console.log(a[n - 1]);
+// }
+// myFunction("abcd", 1);
+
+//------
+
+//Write a function that takes a string (a) as argument. Remove the first 3 characters of a. Return the result
+// function myFunc(a) {
+//   return console.log(a.slice(3));
+// }
+// myFunc("hello");
+
+//------
+
+//Get last n characters of string. Write a function that takes a string as argument. Extract the last 3 characters from the string. Return the result
+// function myFunc(str) {
+//   return console.log(str.slice(-3));
+// }
+// myFunc("world");
